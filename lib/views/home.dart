4d436edd05a8c4935b772a32a235ba778080321d
arun <nuranisafirdausy1201@gmail.com>
2022-04-04 09:01:47 +0700

    2nd

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:wallpaper_app/data/data.dart';
+import 'package:wallpaper_app/model/categories_model.dart';
 import 'package:wallpaper_app/widgets/widget.dart';
+import 'package:http/http.dart' as http;
 
 class Home extends StatefulWidget {
   @override
@@ -7,9 +10,38 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  
+  List<CategorieModel> categories = new List();
+
+  getTrendingWallpaper() async {
+      await http.get(
+          "https://api.pexels.com/v1/curated?per_page=$noOfImageToLoad&page=1",
+          headers: {"Authorization": apiKEY}).then((value) {
+        //print(value.body);
+
+        Map<String, dynamic> jsonData = jsonDecode(value.body);
+        jsonData["photos"].forEach((element) {
+          //print(element);
+          PhotosModel photosModel = new PhotosModel();
+          photosModel = PhotosModel.fromMap(element);
+          photos.add(photosModel);
+          //print(photosModel.toString()+ "  "+ photosModel.src.portrait);
+        });
+
+        setState(() {});
+      });
+    }
+
+  @override
+  void initState(){
+    categories = getCategories();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
         title: brandName(),
         elevation: 0.0,
@@ -18,22 +50,72 @@ class _HomeState extends State<Home> {
         child: Column(
           children: <Widget>[
             Container(
-              
+              decoration: BoxDecoration(
+                color: Color(0xfff5f8fd),
+                borderRadius: BorderRadius.circular(30)
+              ),
+              padding: EdgeInsets.symmetric(horizontal: 24),
+              margin: EdgeInsets.symmetric(horizontal: 24),
               child: Row(
                 children: <Widget>[
                   Expanded(
                     child: TextField(
                       decoration: InputDecoration(
-                        hintText: "search"
+                        hintText: "search wallpaper",
+                        border: InputBorder.none
                       ),
                     ),
                   ),
                   Icon(Icons.search)
                 ],
               ),
+            ),
+            SizedBox(height: 16,),
+            Container(
+              height: 80,
+              child: ListView.builder(
+                padding: EdgeInsets.symmetric(horizontal: 24),
+                itemCount: categories.length,
+                shrinkWrap: true,
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (context, index){
+                  return CategoriesTile(
+                    title: categories[index].categorieName, 
+                    imgUrl: categories[index].imgUrl,
+                  );
+                }
+              ),
             )
           ],
         ),
+      ),
+    );
+  }
+}
+
+class CategoriesTile extends StatelessWidget {
+  
+  final String imgUrl, title;
+  CategoriesTile({@required this.title, @required this.imgUrl});
+  
+  @override
+  Widget build(BuildContext context){
+    return Container(
+      margin: EdgeInsets.only(right: 4),
+      child: Stack(
+        children: <Widget>[
+          ClipRRect(
+            borderRadius: BorderRadius.circular(8),
+            child: Image.network(imgUrl, height: 50, width: 100, fit: BoxFit.contain,)
+          ),
+          Container(
+            color: Colors.black26,
+            height: 50, 
+            width: 100,
+            alignment: Alignment.center,
+            child: Text(title, style: TextStyle(color: Colors.white, fontWeight: FontWeight.w500, fontSize: 15),),
+          )
+        ],
       ),
     );
   }
